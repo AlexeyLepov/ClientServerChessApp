86853d1eccd874c987867b8b6a45c5c767399cae
AlexeyLepov <77492646+AlexeyLepov@users.noreply.github.com>
2022-12-11 21:01:34 +0300

    Update chess.sql

--- MySQL/chess.sql
+++ MySQL/chess.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `chess` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `chess`;
 -- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
 --
 -- Host: localhost    Database: chess
@@ -25,20 +23,16 @@ DROP TABLE IF EXISTS `games`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `games` (
-  `idgames` int NOT NULL AUTO_INCREMENT,
+  `idgames` int NOT NULL,
   `date` datetime DEFAULT NULL,
-  `winner` varchar(1) DEFAULT NULL,
-  `w_player` int DEFAULT NULL,
-  `b_player` int DEFAULT NULL,
+  `winner` varchar(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `w_player` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `b_player` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `moves` int DEFAULT NULL,
   PRIMARY KEY (`idgames`),
-  KEY `w_player_idx` (`w_player`),
-  KEY `b_player_idx` (`b_player`),
   KEY `moves_idx` (`moves`),
-  CONSTRAINT `b_player` FOREIGN KEY (`b_player`) REFERENCES `users` (`idusers`),
-  CONSTRAINT `moves` FOREIGN KEY (`moves`) REFERENCES `moves` (`idmoves`),
-  CONSTRAINT `w_player` FOREIGN KEY (`w_player`) REFERENCES `users` (`idusers`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `moves` FOREIGN KEY (`moves`) REFERENCES `moves` (`idmoves`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -58,12 +52,12 @@ DROP TABLE IF EXISTS `moves`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `moves` (
-  `idmoves` int NOT NULL AUTO_INCREMENT,
+  `idmoves` int NOT NULL,
   `file` blob,
   `w_timleft` time DEFAULT NULL,
   `b_timleft` time DEFAULT NULL,
   PRIMARY KEY (`idmoves`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -83,13 +77,12 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
-  `idusers` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(20) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `password` varchar(20) DEFAULT NULL,
+  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `password` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `score` int DEFAULT NULL,
-  PRIMARY KEY (`idusers`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -98,7 +91,6 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'guest','guest@mail.ru','guest',800);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -111,4 +103,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-11-21 12:31:19
+-- Dump completed on 2022-12-11 20:22:46
