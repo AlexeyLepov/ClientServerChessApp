de9d6e39ec440b308d69323a5b739dd0972ee81e
AlexeyLepov <77492646+AlexeyLepov@users.noreply.github.com>
2022-12-11 16:36:41 +0300

    Update formRegister.py

--- formRegister.py
+++ formRegister.py
@@ -12,33 +12,12 @@ import config
 #                            #
 ##############################
 customtkinter.set_appearance_mode("dark")  # Modes: "System", "Dark", "Light"
-customtkinter.set_default_color_theme("dark-blue")  # Themes: "blue", "green", "dark-blue"
-
+customtkinter.set_default_color_theme("green")  # Themes: "blue", "green", "dark-blue"
 WIDTH = HEIGHT = 350
-
 formRegister = customtkinter.CTk()
 formRegister.geometry(f"{WIDTH+100}x{HEIGHT+100}")
 formRegister.resizable(False, False)
 formRegister.title("Регистрация")
-
-
-try:
-    conn = pymysql.connect(
-        host = config.host,
-        port = config.port,
-        user = config.user,
-        password = config.password,
-        database = config.database,
-        cursorclass = pymysql.cursors.DictCursor
-    )
-    cur = conn.cursor()
-    cur.execute("select @@version")
-    output = cur.fetchall()
-    print(output)
-    print("Connected successfully! ")
-    conn.close()
-except Exception:
-    print("Connection failure ... ")
 
 
 ################################
@@ -48,7 +27,6 @@ except Exception:
 ################################
 def buttonLoginClick():
     ...
-    # conn.close()
 
 def buttonRegisterClick():
     try:
@@ -66,26 +44,39 @@ def buttonRegisterClick():
                 score = 1400
             # INSERT INTO `chess`.`users` (`username`, `email`, `password`, `score`) VALUES ('test', 'test', 'test', '800');
             query = f"INSERT INTO `chess`.`users` (`username`, `email`, `password`, `score`) VALUES ('{entryUser.get()}', '{entryEmail.get()}', '{entryPassword.get()}', '{score}');"
-            
             try:
-                cur.execute(query)
-                if cur:
-                    print(cur)
-                    print("Запись добавлена. ")
-                else:
-                    print(cur)
-                    print("Запись не добавлена.")
-                    labelError.configure(text="Ошибка! ")
-            except (pymysql.Error, pymysql.Warning) as e:
-                print(f'error! {e}')
-            
-            if cur.rowcount == 1:
-                print("Вы успешно зарегистрированы! ")
-                ...
-                conn.close()
-            else:                
-                print("Неверные учетные данные! ")
-                labelError.configure(text="Неверные учетные данные! ")
+                conn = pymysql.connect(
+                    host = config.host,
+                    port = config.port,
+                    user = config.user,
+                    password = config.password,
+                    database = config.database,
+                    cursorclass = pymysql.cursors.DictCursor
+                )
+                cur = conn.cursor()
+                print("Connected successfully! ")
+                try:
+                    cur.execute(query)
+                    if cur:
+                        print(cur)
+                        print("New record added! ")
+                    else:
+                        print(cur)
+                        print("No record has been added ...")
+                        labelError.configure(text="Ошибка! ")
+                except (pymysql.Error, pymysql.Warning) as e:
+                    print(f'error! {e}')
+                    labelError.configure(text=f'Ошибка! {e}')
+                if cur.rowcount == 1:
+                    print("Вы успешно зарегистрированы! ")
+                    ...
+                    conn.close()
+                else:                
+                    print("Неверные учетные данные! ")
+                    labelError.configure(text="Неверные учетные данные! ")
+            except Exception:
+                print("Connection failure ... ")
+                labelError.configure(text="Нет подключения к серверу ... ")
         else:
             print("Пароли должны совпадать! ")
             labelError.configure(text="Пароли должны совпадать! ")
@@ -99,39 +90,60 @@ def buttonRegisterClick():
 #                            #
 ##############################
 frameRegister = customtkinter.CTkFrame(master=formRegister)
-frameRegister.pack(pady=10, padx=10, fill="both", expand=True)
-
 labelWelcome = customtkinter.CTkLabel(master=frameRegister, justify=tkinter.CENTER, width=WIDTH, text="Регистрация")
-labelWelcome.pack(pady=10, padx=10)
-
 entryUser = customtkinter.CTkEntry(master=frameRegister, width=WIDTH, placeholder_text="Введите логин")
-entryUser.pack(pady=10, padx=10)
-
 entryEmail = customtkinter.CTkEntry(master=frameRegister, width=WIDTH, placeholder_text="Введите электронную почту")
-entryEmail .pack(pady=10, padx=10)
-
 entryPassword = customtkinter.CTkEntry(master=frameRegister, width=WIDTH, placeholder_text="Введите пароль", show="*")
-entryPassword.pack(pady=10, padx=10)
-
 entryPasswordRepeat = customtkinter.CTkEntry(master=frameRegister, width=WIDTH, placeholder_text="Подтвердите пароль", show="*")
-entryPasswordRepeat.pack(pady=10, padx=10)
-
 comboboxScore = customtkinter.CTkComboBox(frameRegister, width=WIDTH, values=["Новичок", "Любитель", "Мастер"])
-comboboxScore.pack(pady=12, padx=10)
-
 buttonRegister = customtkinter.CTkButton(master=frameRegister, width=WIDTH, text="Зарегистрироваться", command=buttonRegisterClick)
-buttonRegister.pack(pady=10, padx=10)
-
 buttonLogin = customtkinter.CTkButton(master=frameRegister, width=WIDTH, text="Войти (если уже есть аккаунт)", command=buttonLoginClick)
-buttonLogin.pack(pady=10, padx=10)
-
 labelError = customtkinter.CTkLabel(master=frameRegister, width=WIDTH, justify=tkinter.CENTER, text="")
-labelError.pack(pady=10, padx=10)
+frameRegister.pack(
+    pady=10, padx=10, fill="both", expand=True)
+labelWelcome.pack(
+    pady=10, padx=10)
+entryUser.pack(
+    pady=10, padx=10)
+entryEmail .pack(
+    pady=10, padx=10)
+entryPassword.pack(
+    pady=10, padx=10)
+entryPasswordRepeat.pack(
+    pady=10, padx=10)
+comboboxScore.pack(
+    pady=12, padx=10)
+buttonRegister.pack(
+    pady=10, padx=10)
+buttonLogin.pack(
+    pady=10, padx=10)
+labelError.pack(
+    pady=10, padx=10)
 
 
-#############################
-#                           #
-#    Form initialization    #
-#                           #
-#############################
+################################
+#                              #
+#    Test connection to DB     #
+#                              #
+################################
+try:
+    conn = pymysql.connect(
+        host = config.host,
+        port = config.port,
+        user = config.user,
+        password = config.password,
+        database = config.database,
+        cursorclass = pymysql.cursors.DictCursor
+    )
+    cur = conn.cursor()
+    cur.execute("select @@version")
+    output = cur.fetchall()
+    print(output)
+    print("Connected successfully! ")
+    conn.close()
+except Exception:
+    print("Connection failure ... ")
+    labelError.configure(text="Нет подключения к серверу ... ")
+
+
 formRegister.mainloop()
