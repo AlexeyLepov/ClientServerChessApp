8a918b12a839cc0b81e0b1620623f8b3caadc7db
AlexeyLepov <77492646+AlexeyLepov@users.noreply.github.com>
2022-12-11 16:36:38 +0300

    Update formLogin.py

--- formLogin.py
+++ formLogin.py
@@ -12,67 +12,12 @@ import config
 #                            #
 ##############################
 customtkinter.set_appearance_mode("dark")  # Modes: "System", "Dark", "Light"
-customtkinter.set_default_color_theme("dark-blue")  # Themes: "blue", "green", "dark-blue"
-
+customtkinter.set_default_color_theme("green")  # Themes: "blue", "green", "dark-blue"
 WIDTH = HEIGHT = 240
-
 formLogin = customtkinter.CTk()
 formLogin.geometry(f"{WIDTH+100}x{HEIGHT+100}")
+formLogin.resizable(False, False)
 formLogin.title("Вход в систему")
-
-
-##############################
-#                            #
-#    Building frame items    #
-#                            #
-##############################
-frameLogin = customtkinter.CTkFrame(master=formLogin)
-frameLogin.pack(pady=10, padx=10, fill="both", expand=True)
-
-labelWelcome = customtkinter.CTkLabel(master=frameLogin, justify=tkinter.CENTER, width=WIDTH, text="Вход в систему")
-labelWelcome.pack(pady=10, padx=10)
-
-entryUser = customtkinter.CTkEntry(master=frameLogin, width=WIDTH, placeholder_text="Введите логин")
-entryUser.pack(pady=10, padx=10)
-
-entryPassword = customtkinter.CTkEntry(master=frameLogin, width=WIDTH, placeholder_text="Введите пароль", show="*")
-entryPassword.pack(pady=10, padx=10)
-
-buttonLogin = customtkinter.CTkButton(master=frameLogin, text="Войти", width=WIDTH)
-buttonLogin.pack(pady=10, padx=10)
-
-buttonGuest = customtkinter.CTkButton(master=frameLogin, text="Войти как гость", width=WIDTH)
-buttonGuest.pack(pady=10, padx=10)
-
-buttonRegister = customtkinter.CTkButton(master=frameLogin, text="Зарегистрироваться", width=WIDTH)
-buttonRegister.pack(pady=10, padx=10)
-
-labelError = customtkinter.CTkLabel(master=frameLogin, justify=tkinter.CENTER, width=WIDTH, text="")
-labelError.pack(pady=10, padx=10)
-
-
-##########################
-#                        #
-#    Connecting to DB    #
-#                        #
-##########################
-try:
-    conn = pymysql.connect(
-        host = config.host,
-        port = config.port,
-        user = config.user,
-        password = config.password,
-        database = config.database,
-        cursorclass = pymysql.cursors.DictCursor
-    )
-    cur = conn.cursor()
-    cur.execute("select @@version")
-    output = cur.fetchall()
-    print(output)
-    print("Connected successfully! ")
-except Exception:
-    print("Connection failure ... ")
-    labelError.configure(text="Нет подключения к серверу... ")
 
 
 ################################
@@ -114,14 +59,62 @@ def buttonRegisterClick():
     ...
 
 
-buttonLogin.command = lambda: buttonLoginClick()
-buttonGuest.command = lambda: buttonGuestClick()
-buttonRegister.command = lambda: buttonRegisterClick()
+##############################
+#                            #
+#    Building frame items    #
+#                            #
+##############################
+#creating elements
+frameLogin = customtkinter.CTkFrame(master=formLogin)
+labelWelcome = customtkinter.CTkLabel(master=frameLogin, justify=tkinter.CENTER, width=WIDTH, text="Вход в систему")
+entryUser = customtkinter.CTkEntry(master=frameLogin, width=WIDTH, placeholder_text="Введите логин")
+entryPassword = customtkinter.CTkEntry(master=frameLogin, width=WIDTH, placeholder_text="Введите пароль", show="*")
+buttonLogin = customtkinter.CTkButton(master=frameLogin, text="Войти", width=WIDTH, command = lambda: buttonLoginClick())
+buttonGuest = customtkinter.CTkButton(master=frameLogin, text="Войти как гость", width=WIDTH, command = lambda: buttonGuestClick())
+buttonRegister = customtkinter.CTkButton(master=frameLogin, text="Зарегистрироваться", width=WIDTH, command = lambda: buttonRegisterClick())
+labelError = customtkinter.CTkLabel(master=frameLogin, justify=tkinter.CENTER, width=WIDTH, text="")
+#packing elements
+frameLogin.pack(
+    pady=10, padx=10, fill="both", expand=True)
+labelWelcome.pack(
+    pady=10, padx=10)
+entryUser.pack(
+    pady=10, padx=10)
+entryPassword.pack(
+    pady=10, padx=10)
+buttonLogin.pack(
+    pady=10, padx=10)
+buttonGuest.pack(
+    pady=10, padx=10)
+buttonRegister.pack(
+    pady=10, padx=10)
+labelError.pack(
+    pady=10, padx=10)
 
 
-#############################
-#                           #
-#    Form initialization    #
-#                           #
-#############################
+################################
+#                              #
+#    Test connection to DB     #
+#                              #
+################################
+try:
+    conn = pymysql.connect(
+        host = config.host,
+        port = config.port,
+        user = config.user,
+        password = config.password,
+        database = config.database,
+        cursorclass = pymysql.cursors.DictCursor
+    )
+    cur = conn.cursor()
+    cur.execute("select @@version")
+    output = cur.fetchall()
+    print(output)
+    print("Connected successfully! ")
+    conn.close()
+except Exception:
+    print("Connection failure ... ")
+    labelError.configure(text="Нет подключения к серверу ... ")
+
+
 formLogin.mainloop()
